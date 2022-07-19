99a498bb09abd4824c778d9e7f27aad1e1d448ba
Jed Mitten <jedmitten+githubconfig@gmail.com>
2022-07-19 09:31:19 -0700

    updates to get init to run

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ source .venv/bin/activate
 
 # setup database if it does not exist with an admin and standard users
 
-DB_FILE="vuln_app.db"
+DB_FILE="./project/vuln_app.db"
 
 if [ ! -f "$DB_FILE" ]; then
     echo "Initializing database..."
@@ -13,3 +13,5 @@ fi
 if [ -f "$DB_FILE" ]; then
     echo "DB file exists: $DB_FILE"
 fi
+
+echo "Project initialization complete!"
